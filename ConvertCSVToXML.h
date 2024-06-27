b00760da9ad470a7293bf3be65bc2af84fd01aca
kgurumoorthi <gurumoorthi2@gmail.com>
2024-06-27 22:29:04 +0530

    Update ConvertCSVToXML.h

--- ConvertCSVToXML.h
+++ ConvertCSVToXML.h
@@ -2,6 +2,12 @@
 #include "StringHelpers.h"
 #include "TradeRecord.h"
 
+#define MAX_LINE_LENGTH 1024
+#define NUM_FIELDS 3
+#define CURRENCY_LENGTH 6
+#define MAX_TRADE_OBJECTS 1024
+#define XML_FILENAME "output.xml"
+
 bool validateTradeLine(char* line, int lineCount);
 void parseTradeLine(char* line, TradeRecord* tradeObjects, int* objectCount, int* lineCount);
 void writeTradeRecordsToXML(const TradeRecord* tradeObjects, int objectCount);
