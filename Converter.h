a3f3241b4fd44061cbfa2f44952e5afde369c6ca
kgurumoorthi <gurumoorthi2@gmail.com>
2024-06-27 22:00:56 +0530

    Update Converter.h

--- Converter.h
+++ Converter.h
@@ -1,1 +1,17 @@
+bool TryParseIntFromString(const char* string, int* value) {
+    char* endptr;
+    *value = strtol(string, &endptr, 10);
+    if (endptr == string) {
+        return False; 
+    }
+    return True; 
+}
 
+bool TryParseDoubleFromString(const char* string, double* value) {
+    char* endptr;
+    *value = strtod(string, &endptr);
+    if (endptr == string) {
+        return False; 
+    }
+    return True; 
+}
