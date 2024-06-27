44d1f12f7816cd460cee647a518aed15c03ca463
kgurumoorthi <gurumoorthi2@gmail.com>
2024-06-27 22:01:44 +0530

    Update ConvertCSVToXML.h

--- ConvertCSVToXML.h
+++ ConvertCSVToXML.h
@@ -1,3 +1,15 @@
 #include "Converter.h"
 #include "StringHelpers.h"
 #include "TradeRecord.h"
+
+#define MAX_LINE_LENGTH 1024
+#define MAX_TRADE_RECORDS 1024
+#define CURRENCY_CODE_LENGTH 3
+#define CURRENCY_PAIR_LENGTH 6
+#define LOT_SIZE 100000
+
+bool ValidateTradeTokens(char* fields[CSV_FIELDS], int lineCount);
+bool ConvertTokensToTradeRecord(char* fields[CSV_FIELDS], TradeRecord* tradeRecord);
+bool ParseTradeLine(const char* line, TradeRecord* tradeRecord, int lineCount);
+void WriteTradesToXML(const TradeRecord* tradeRecords, int count, const char* outputFilePath);
+void ConvertCSVToXML(FILE* inputFile, const char* outputFilePath);
